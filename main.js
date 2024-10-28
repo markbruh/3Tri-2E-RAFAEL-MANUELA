7c3340ceeaf83c04ed30094fc22b407e123ff971
markbruh <160138553+markbruh@users.noreply.github.com>
2024-10-28 08:53:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function criaCartao(categoria,pergunta,resposta){
+  let container = document.getElementById("container")
+  let article = document.createElement("article")
+  article.className = "cartao"
+  article.innerHTML = 'isso é um teste'
+container.appendChild(article)
+}
+
+criaCartao("Biologia","Qual a função da mitocondria?","Produzir energia (ATP).")
+
+
