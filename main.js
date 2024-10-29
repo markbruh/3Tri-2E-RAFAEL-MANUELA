3ed3f82869bbc671377eac967d6275ae488735c2
markbruh <160138553+markbruh@users.noreply.github.com>
2024-10-29 08:51:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,19 @@ function criaCartao(categoria,pergunta,resposta){
   let container = document.getElementById("container")
   let article = document.createElement("article")
   article.className = "cartao"
-  article.innerHTML = 'isso é um teste'
+  article.innerHTML = `
+  <div class = "cartao-conteudo">
+  <h3> ${categoria} </h3>
+  <div class = "cartao-conteudo-pergunta">
+  <p> ${pergunta} </p>
+  </div>
+  <div class = "cartao-conteudo-resposta">
+  <p> ${resposta} </p>
+  </div>
+  </div>
+  `
 container.appendChild(article)
 }
-
-criaCartao("Biologia","Qual a função da mitocondria?","Produzir energia (ATP).")
-
+criaCartao("História","O que foi o nazismo?","Foi ideologia metapolítica à favor da prevalência da raça ariana.") 
+criaCartao("Literatura","Quem foi Thomas More?","Escritor da obra utopia, muito utilizado nas redações de vestibulares.")
 
